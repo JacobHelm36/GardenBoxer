628b6cf57fbb880b359d954cc86d0b282b375ea4
BradleyG248 <calvinandhobbs28@gmail.com>
2020-04-27 12:01:12 -0600

    Added models

---
+++ Models/Garden.cs
@@ -0,0 +1,14 @@
+namespace Keepr.Models
+{
+  public class Garden
+  {
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+
+  }
+}
