39ff3b1bef8942f924747231edb69b349d38982b
BradleyG248 <calvinandhobbs28@gmail.com>
2020-04-27 11:21:05 -0600

    auth

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "ten-1085.auth0.com";
+export const audience = "bradleyslibrary.com";
+export const clientId = "xaMXmVh3aIuG72q0WNDsoO2LP962x4Qk";
