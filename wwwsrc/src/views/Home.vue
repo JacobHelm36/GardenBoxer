0305eec03e2541d3a89377035efcceca892535ba
BradleyG248 <calvinandhobbs28@gmail.com>
2020-05-06 16:00:55 -0600

    home page

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="home">
-    <h1>Garden Boxer</h1>
-    <h3>About:</h3>
+  <div class="home container">
+    <div class="row">
+      <div class="col-12 mb-3 d-flex justify-content-center text-center">
+        <h1>Garden Boxer</h1>
+      </div>
+      <div class="col-6 d-flex justify-content-end align-items-center">
+        <h3 class="mb-0">About:</h3>
+      </div>
+      <div class="col-6 d-flex justify-content-start align-items-center">
+        <p class="mb-0">We're not a company and we don't have a mission, but you know, why not.</p>
+      </div>
+    </div>
   </div>
 </template>
 
